b59dff180611289bfef8e62e1cbc07f8a503a0ef
jyuill <john@fig4.com>
2019-02-18 16:34:54 -0800

    setting up to post R markdown to Wordpress. Initial experiment positive - some challenges with figures but they can be worked out.

---
+++ Package-setup.R
@@ -0,0 +1,15 @@
+## Packages needed to post R markdown on Wordpress Blog
+
+## References:
+## http://tabvizexplorer.com/how-to-upload-r-markdown-directly-to-wordpress/
+
+## Packages
+library(devtools)
+library(RCurl)
+library(XML)
+
+#install_github('duncantl/XMLRPC')
+#install_github('duncantl/RWordPress')
+
+library(XMLRPC)
+library(RWordPress)
---
+++ Upload-to-Wordpress.R
@@ -0,0 +1,23 @@
+## Upload Blog Post and R Markdown to Wordpress Blog
+
+library(RWordPress)
+library(knitr)
+
+options(WordpressLogin= c(johnyuill='Radius301!'),
+       WordpressURL='https://catbirdanalytics.wordpress.com/xmlrpc.php')
+
+# Upload plots: set knitr options
+## this works but:
+## - saves files in a folder in WordPress
+## http://catbirdanalytics.files.wordpress.com/YYYY/MM/<name of code chunk>-1.png
+## throws Warnings about deprecated functions :(
+#opts_knit$set(upload.fun = function(file){library(RWordPress);uploadFile(file)$url;})
+
+knit2wp('test-r-markdown-wordpress.Rmd', 
+        title = 'testing-r-markdown-wordpress', 
+        publish = FALSE) # your filename and blog post title
+
+## alternative approach to figures:
+## 1. use ggsave to save charts with desired name/location locally (at least for ggplot2)
+## 2. use googledrive to upload file to GDrive
+## 3. embed link to GDrive location
